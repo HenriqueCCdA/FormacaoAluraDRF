60890fa542b6d9304d0102ec7219e3560bcdb5b2
Henrique C. C. de Andrade <henrique.ccda@gmail.com>
2024-08-20 11:09:58 -0300

    Curso 1 - Fim

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # FormacaoAluraDRF
 
-1. Curso de Django REST Framework: construindo APIs RESTful do Zero - Inicio: 19/08/2024
+1. Curso de Django REST Framework: construindo APIs RESTful do Zero - Inicio: 19/08/2024 - Fim: 20/08/2024
--- curso1/escola/views.py
+++ curso1/escola/views.py
@@ -7,23 +7,28 @@ from escola.serializers import (
     MatriculaSerializer,
 )
 from rest_framework import viewsets, generics
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 
 
 class EstudanteViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = Estudante.objects.all()
     serializer_class = EstudanteSerializer
 
 
 class CursoViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = Curso.objects.all()
     serializer_class = CursoSerializer
 
 
 class MatriculaViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = Matricula.objects.all()
     serializer_class = MatriculaSerializer
 
 class ListaMatriculaEstudante(generics.ListAPIView):
+    permission_classes = [IsAuthenticated]
     def get_queryset(self):
         queryset = Matricula.objects.filter(estudante_id=self.kwargs['pk'])
         return queryset
@@ -31,6 +36,7 @@ class ListaMatriculaEstudante(generics.ListAPIView):
     serializer_class  = ListaMatriculasEstudanteSerializer
 
 class ListaMatriculaCurso(generics.ListAPIView):
+    permission_classes = [IsAuthenticated]
     def get_queryset(self):
         queryset = Matricula.objects.filter(curso_id=self.kwargs['pk'])
         return queryset
