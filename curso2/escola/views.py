6342b275cad340eb8cf365bca9acee9299c0ee31
Henrique C. C. de Andrade <henrique.ccda@gmail.com>
2024-08-29 17:42:28 -0300

    Curso 2 0 Aula 4

---
+++ curso2/escola/views.py
@@ -0,0 +1,48 @@
+from escola.models import Estudante, Curso, Matricula
+from escola.serializers import (
+    EstudanteSerializer,
+    CursoSerializer,
+    ListaMatriculasCursoSerializer,
+    ListaMatriculasEstudanteSerializer,
+    MatriculaSerializer,
+)
+from rest_framework import viewsets, generics, filters
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from django_filters.rest_framework import DjangoFilterBackend
+
+
+class EstudanteViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    queryset = Estudante.objects.all()
+    serializer_class = EstudanteSerializer
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter]
+    ordering_fields = ['nome']
+    search_fields = ['nome', 'cpf']
+
+
+class CursoViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
+
+
+class MatriculaViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
+    queryset = Matricula.objects.all()
+    serializer_class = MatriculaSerializer
+
+class ListaMatriculaEstudante(generics.ListAPIView):
+    permission_classes = [IsAuthenticated]
+    def get_queryset(self):
+        queryset = Matricula.objects.filter(estudante_id=self.kwargs['pk'])
+        return queryset
+
+    serializer_class  = ListaMatriculasEstudanteSerializer
+
+class ListaMatriculaCurso(generics.ListAPIView):
+    permission_classes = [IsAuthenticated]
+    def get_queryset(self):
+        queryset = Matricula.objects.filter(curso_id=self.kwargs['pk'])
+        return queryset
+
+    serializer_class = ListaMatriculasCursoSerializer
